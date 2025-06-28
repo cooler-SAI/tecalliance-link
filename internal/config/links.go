72199e29ffd92c72fed936aebdc1cbb12a04f050
cooler-SAI <coolerme86@gmail.com>
2025-06-28 20:39:05 +0300

    docs(readme): Add details on running the application
    Adds a new section to the README describing the application's
    behavior upon execution.
    
    This section clarifies that the application:
    - Starts a local web server on http://localhost:8095.
    - Automatically opens the URL in the default browser.
    - Prevents multiple instances from running simultaneously.

--- internal/config/links.go
+++ internal/config/links.go
@@ -9,6 +9,7 @@ var Links = []Link{
 	{Name: "Ajusa", URL: "https://web.tecalliance.net/ajusa/en/home"},
 	{Name: "Ate", URL: "https://web.tecalliance.net/ate/en/home"},
 	{Name: "Bosal", URL: "https://web.tecalliance.net/bosal/en/home"},
+	{Name: "Bosch", URL: "https://web.tecalliance.net/bosch-catalog/qb/home"},
 	{Name: "Continental", URL: "https://web.tecalliance.net/continental/en/home"},
 	{Name: "Elring", URL: "https://web.tecalliance.net/elring/en/home"},
 	{Name: "FAE", URL: "https://web.tecalliance.net/fae/en/home"},
@@ -16,6 +17,7 @@ var Links = []Link{
 	{Name: "Kayaba", URL: "https://web.tecalliance.net/kyb/en/home"},
 	{Name: "MAHLE", URL: "https://web.tecalliance.net/mahle-catalog/en/home"},
 	{Name: "NGK", URL: "https://web.tecalliance.net/ngk-catalog/en/home"},
+	{Name: "Seinsa", URL: "https://web.tecalliance.net/seinsa/en/home"},
 	{Name: "Stellox", URL: "https://web.tecalliance.net/stellox/en/home"},
 	{Name: "Victor Reinz", URL: "https://web.tecalliance.net/reinz/en/home"},
 }
